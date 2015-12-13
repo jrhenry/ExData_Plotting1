65037c96aa64b5996d012a75f838fdf20d8bdfb6
jrhenry <james.r.henry1@gmail.com>
2015-12-12 23:46:39 -0500

    plot1.R

---
+++ plot1.R
@@ -0,0 +1,19 @@
+# Loads the data
+dset<- read.table("household_power_consumption.txt", col.names = 
+                  c("Date", "Time","Global_active_power","Global_reactive_power",
+                  "Voltage","Global_intensity","Sub_metering_1",
+                  "Sub_metering_2","Sub_metering_3"), sep = ";", na.strings = "?",
+                  skip = 66637, nrows = 2880)
+
+# Converts date and time into single column and POSIXlt class
+dset$Date<-strptime(paste(dset$Date,dset$Time), "%d/%m/%Y %H:%M:%S")
+
+# Opens png file for writing
+png(filename = "plot1.png", width = 480, height = 480, units = "px", res = NA, bg= "white")
+
+# Builds a histogram ofr the global acvtive power in kilowatts
+hist(dset$Global_active_power, col = "red", main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)")
+
+# Closes png file
+dev.off()
